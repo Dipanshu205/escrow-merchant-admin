b1f35522166dd20c5c0326f2d4faca68b4be4909
Dipanshu205 <dipanshuchoudhary205@gmail.com>
2026-02-22 16:09:37 +0530

    first commit

---
+++ MOrderSecret.jsx
@@ -0,0 +1,12 @@
+// import jwt from 'jsonwebtoken';
+
+
+
+// const onhandlesubmit =()=> {
+
+//     const token = localStorage.getItem("token");
+//     const JWT_SECRET = import.meta.env.VITE_JWT_SECRET;
+//     const token_decode = jwt.verify(token, JWT_SECRET);
+//     merchantId = token_decode.merchantId;
+
+// }
